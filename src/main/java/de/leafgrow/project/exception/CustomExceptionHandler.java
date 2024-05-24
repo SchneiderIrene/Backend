78f4e023b619c50c2f30d0eaad62ad775be48e49
chicagoist <le.legioner@gmail.com>
2024-05-25 01:54:07 +0200

    fixes CustomExceptionHandler.

---
+++ src/main/java/de/leafgrow/project/exception/CustomExceptionHandler.java
@@ -0,0 +1,20 @@
+package de.leafgrow.project.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<String> handleAllExceptions(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
