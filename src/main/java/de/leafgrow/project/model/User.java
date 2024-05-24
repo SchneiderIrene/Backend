41c5f05c816a8ebe36d90b3318eee93b14ef55f0
chicagoist <le.legioner@gmail.com>
2024-05-25 01:33:58 +0200

    added User with import jakarta.persistence.*;

---
+++ src/main/java/de/leafgrow/project/model/User.java
@@ -0,0 +1,30 @@
+package de.leafgrow.project.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+//import javax.persistence.*;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
+
+    @OneToMany(mappedBy = "user")
+    private List<Pot> pots;
+}
