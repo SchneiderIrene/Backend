f827d741695c0df6469e9992544e0eba45db2f88
chicagoist <le.legioner@gmail.com>
2024-05-25 01:19:43 +0200

    added AuthController

---
+++ src/main/java/de/leafgrow/project/controller/AuthController.java
@@ -0,0 +1,30 @@
+package de.leafgrow.project.controller;
+
+import de.leafgrow.project.dto.UserLoginDto;
+import de.leafgrow.project.dto.UserRegistrationDto;
+import de.leafgrow.project.security.JwtTokenUtil;
+import de.leafgrow.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody UserRegistrationDto userRegistrationDto) {
+        return ResponseEntity.ok(userService.register(userRegistrationDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody UserLoginDto authenticationRequest) throws Exception {
+        return ResponseEntity.ok(userService.authenticate(authenticationRequest));
+    }
+}
