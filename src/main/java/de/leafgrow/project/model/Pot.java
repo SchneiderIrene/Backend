ad0889bb9a71a04b2a349caa368acae1bad2d823
chicagoist <le.legioner@gmail.com>
2024-05-25 01:29:28 +0200

    added Pot

---
+++ src/main/java/de/leafgrow/project/model/Pot.java
@@ -0,0 +1,24 @@
+package de.leafgrow.project.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Pot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String plantName;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private boolean active;
+}
