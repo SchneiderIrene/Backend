1159b0b5e12aafcc92744e5fa4b747149ad4e23f
chicagoist <le.legioner@gmail.com>
2024-05-25 02:22:04 +0200

    fixes JwtRequestFilter

--- src/main/java/de/leafgrow/project/security/JwtRequestFilter.java
+++ src/main/java/de/leafgrow/project/security/JwtRequestFilter.java
@@ -25,6 +25,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
 
+    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
 
