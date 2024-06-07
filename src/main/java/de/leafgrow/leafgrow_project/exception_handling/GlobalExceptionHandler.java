1e73ee89600112e1bd96dde0dbcfe5c4890345e0
chicagoist <z7t35gbba@mozmail.com>
2024-06-07 13:32:47 +0200

    Default implementation of class GlobalExceptionHandler

--- src/main/java/de/leafgrow/leafgrow_project/exception_handling/GlobalExceptionHandler.java
+++ src/main/java/de/leafgrow/leafgrow_project/exception_handling/GlobalExceptionHandler.java
@@ -1,4 +1,24 @@
 package de.leafgrow.leafgrow_project.exception_handling;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(new Response("Internal server error: " + ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Object> handleResponseStatusException(ResponseStatusException ex, WebRequest request) {
+        return new ResponseEntity<>(new Response("Error: " + ex.getReason()),
+                ex.getStatusCode());
+    }
 }
