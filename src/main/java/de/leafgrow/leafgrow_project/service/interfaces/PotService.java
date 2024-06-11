9b230a2690109ad055311e3144a6536476747ce7
chicagoist <z7t35gbba@mozmail.com>
2024-06-11 15:45:50 +0200

    after Vasilli predeploy

--- src/main/java/de/leafgrow/leafgrow_project/service/interfaces/PotService.java
+++ src/main/java/de/leafgrow/leafgrow_project/service/interfaces/PotService.java
@@ -1,6 +1,5 @@
 package de.leafgrow.leafgrow_project.service.interfaces;
 
-import de.leafgrow.leafgrow_project.domain.entity.Instruction;
 import de.leafgrow.leafgrow_project.domain.entity.Pot;
 import de.leafgrow.leafgrow_project.domain.entity.User;
 
@@ -13,13 +12,10 @@ public interface PotService {
 
     Pot activatePot(Long potId);
 
-    void skipDay(Pot pot);
+    //void skipDay(Pot pot);
 
     List<Pot> findPotsByUserId(Long userId);
 
 
     void skipDay(Pot pot);
-
-    List<Pot> findPotsByUserId(Long userId);
-
 }
